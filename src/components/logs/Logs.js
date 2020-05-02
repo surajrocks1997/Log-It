fcd52369bba447e1d806137e24974728361d1301
surajrocks1997 <surajrocks1997@gmail.com>
2020-05-02 17:00:46 +0530

    working on logitem component, Moment library causing error

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 
+import LogItem from "./LogItem";
+
 const Logs = () => {
   const [logs, setLogs] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -23,14 +25,14 @@ const Logs = () => {
   }
 
   return (
-    <ul className="collection-with-header">
+    <ul className="collection with-header">
       <li className="collection-header">
         <h4 className="center">System Logs</h4>
       </li>
       {!loading && logs.length === 0 ? (
         <p className="center">No Logs to show...</p>
       ) : (
-        logs.map((log) => <li>{log.message}</li>)
+        logs.map((log) => <LogItem log={log} key={log.id} />)
       )}
     </ul>
   );

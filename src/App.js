74ae932005e589f5797754d7a386d061d752e4a1
surajrocks1997 <surajrocks1997@gmail.com>
2020-05-03 16:17:01 +0530

    edit and tech modal components added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import SearchBar from "./components/layout/SearchBar";
 import Logs from "./components/logs/Logs";
 import AddBtn from "./components/layout/AddBtn";
 import AddLogModal from "./components/logs/AddLogModal";
+import EditLogModal from "./components/logs/EditLogModal";
+import AddTechModal from "./components/techs/AddTechModal";
+import TechListModal from "./components/techs/TechListModal";
 
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
@@ -16,14 +19,19 @@ const App = () => {
     M.AutoInit();
   });
 
-  return <Fragment>
-    <SearchBar />
-    <div className="container">
-      <AddBtn />
-      <AddLogModal />
-      <Logs />
-    </div>
-  </Fragment>;
+  return (
+    <Fragment>
+      <SearchBar />
+      <div className="container">
+        <AddBtn />
+        <AddLogModal />
+        <EditLogModal />
+        <AddTechModal />
+        <TechListModal />
+        <Logs />
+      </div>
+    </Fragment>
+  );
 };
 
 export default App;

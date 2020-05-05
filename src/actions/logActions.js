cd1b93b4a491813a6b492888f292012c0ab1f95b
surajrocks1997 <surajrocks1997@gmail.com>
2020-05-05 17:21:19 +0530

    search logs functionality added

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -7,6 +7,7 @@ import {
   SET_CURRENT,
   CLEAR_CURRENT,
   UPDATE_LOG,
+  SEARCH_LOGS,
 } from "./types";
 
 // get logs from server
@@ -93,6 +94,26 @@ export const updateLog = (log) => async (dispatch) => {
 
     dispatch({
       type: UPDATE_LOG,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data,
+    });
+  }
+};
+
+// search logs from server
+export const searchLogs = (text) => async (dispatch) => {
+  try {
+    setLoading();
+
+    const res = await fetch(`/logs?q=${text}`);
+    const data = await res.json();
+
+    dispatch({
+      type: SEARCH_LOGS,
       payload: data,
     });
   } catch (err) {
